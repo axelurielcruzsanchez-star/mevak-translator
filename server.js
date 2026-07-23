fe8728caba1177195c9ba327b63a6898345ad229
Axel Uriel Cruz Sanchez <axelurielcruzsanchez@gmail.com>
2026-07-22 22:43:17 -0600

    Actualizar servidor de traducción

--- server.js
+++ server.js
@@ -7,13 +7,19 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// Ruta principal
 app.get("/", (req, res) => {
     res.send("✅ Mevak Translator funcionando");
 });
 
+// Ruta para traducir
 app.post("/translate", async (req, res) => {
+
     try {
+
         const { text } = req.body;
+
+        console.log("Texto recibido:", text);
 
         const response = await axios.post(
             "https://libretranslate.de/translate",
@@ -30,16 +36,22 @@ app.post("/translate", async (req, res) => {
             }
         );
 
+        console.log(response.data);
+
         res.json({
             translatedText: response.data.translatedText
         });
 
     } catch (error) {
-        console.error(error.message);
+
+        console.error(error.response?.data || error.message);
+
         res.status(500).json({
             error: "Error al traducir"
         });
+
     }
+
 });
 
 const PORT = process.env.PORT || 3000;
